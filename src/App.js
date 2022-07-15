f0c2becf15ba14648f0fd000e58d242166fd550e
Anchal Gupta <37269403+anchal-gupta@users.noreply.github.com>
2022-07-15 12:57:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Main from './components/layout/Main';
+
+function App() {
+  return (
+    <div className="App">
+  <Header />
+  <Main />
+  <Footer/>
+    </div>
+  );
+}
+
+export default App;
